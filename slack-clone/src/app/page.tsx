5a70d5a0e47b6083ef616ef4293670b2e70e035a
chinel <nelokwaz@gmail.com>
2024-10-30 21:16:25 +0100

    fix: fix github signout issue

--- slack-clone/src/app/page.tsx
+++ slack-clone/src/app/page.tsx
@@ -2,14 +2,21 @@
 
 import { Button } from "@/components/ui/button";
 import { useAuthActions } from "@convex-dev/auth/react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
   const { signOut } = useAuthActions();
+  const router = useRouter();
+
+  const handleSignOut = async () => {
+    await signOut();
+    router.push("/auth");
+  };
 
   return (
     <div>
       <p>Logged in</p>
-      <Button onClick={() => signOut()}>Sign Out</Button>
+      <Button onClick={handleSignOut}>Sign Out</Button>
     </div>
   );
 }
