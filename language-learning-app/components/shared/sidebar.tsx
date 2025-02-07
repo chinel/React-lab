6c1d17f4d826a1de01ab12a9887c9d2f95b5ebab
chinel <nelokwaz@gmail.com>
2025-02-07 12:38:05 +0100

    feat: add sidebar logo & items

--- language-learning-app/components/shared/sidebar.tsx
+++ language-learning-app/components/shared/sidebar.tsx
@@ -1,4 +1,6 @@
 import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
 
 type Props = {
   className?: string;
@@ -8,11 +10,21 @@ const Sidebar = ({ className }: Props) => {
   return (
     <div
       className={cn(
-        "flex bg-blue-500 h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
+        "flex h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
         className
       )}
     >
-      Side bar
+      <Link href="/learn">
+        <div className="pt-7 pl-4 pb-7 flex items-center gap-x-3">
+          <Image src="/mascot.svg" height={40} width={40} alt="Lingo" />
+          <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
+            Lingo
+          </h1>
+        </div>
+      </Link>
+      <div className="flex flex-col gap-y-2 flex-1">
+        <SidebarItem />
+      </div>
     </div>
   );
 };
