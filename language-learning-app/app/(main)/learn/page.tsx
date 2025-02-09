8d64ce913e804de0bcc4737ad739de79364de1fc
chinel <nelokwaz@gmail.com>
2025-02-09 15:32:34 +0100

    feat: create learn page wrapper & components

--- language-learning-app/app/(main)/learn/page.tsx
+++ language-learning-app/app/(main)/learn/page.tsx
@@ -1,9 +1,14 @@
+import { FeedWrapper, StickyWrapper } from "@/components/shared";
 import React from "react";
+import { Header } from "./components";
 
 const LearnPage = () => {
   return (
-    <div>
-      <h4>Learn Page</h4>
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+      <StickyWrapper>My Sticky sidebar</StickyWrapper>
+      <FeedWrapper>
+        <Header title="Spanish" />
+      </FeedWrapper>
     </div>
   );
 };
