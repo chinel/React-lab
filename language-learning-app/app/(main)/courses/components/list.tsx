3e52c8679feb380f32bc10da7f6834fa5e8862e4
chinel <nelokwaz@gmail.com>
2025-02-14 23:56:53 +0100

    feat: create server actions for user progress

--- language-learning-app/app/(main)/courses/components/list.tsx
+++ language-learning-app/app/(main)/courses/components/list.tsx
@@ -2,6 +2,8 @@
 
 import { courses, userProgress } from "@/db/schema";
 import Card from "./card";
+import { useRouter } from "next/navigation"; // this is the new import the old import is next router
+import { useTransition } from "react";
 
 type Props = {
   courses: (typeof courses.$inferSelect)[];
@@ -9,6 +11,21 @@ type Props = {
 };
 
 const List = ({ courses, activeCourseId }: Props) => {
+  const router = useRouter();
+  const [pending, startTransition] = useTransition(); //this will help us use a server action & its pending state
+
+  const onClick = (id: number) => {
+    if (pending) {
+      return;
+    }
+    if (id === activeCourseId) {
+      return router.push("/learn");
+    }
+
+    startTransition(() => {
+      router.push(`/courses/${id}`);
+    });
+  };
   return (
     <div className="pt-6 grid grid-cols-2 lg:grid-cols-[repeat(auto-fill,minmax(210px,1fr))] gap-4">
       {courses.map((course) => (
