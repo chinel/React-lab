fcf794fef5a3bc85d32327e12f1180a2595b7e53
chinel <nelokwaz@gmail.com>
2025-02-19 22:36:38 +0100

    feat: update seed script

--- language-learning-app/db/schema/index.ts
+++ language-learning-app/db/schema/index.ts
@@ -1,2 +1,13 @@
 export { courses, coursesRelations } from "./courses";
 export { userProgress, userProgressRelations } from "./user-progress";
+export { units, unitsRelations } from "./units";
+export { lessons, lessonsRelations } from "./lessons";
+export { challenges, challengesRelations } from "./challenges";
+export {
+  challengeOptions,
+  challengeOptionsRelations,
+} from "./challengeOptions";
+export {
+  challengeProgress,
+  challengeProgressRelations,
+} from "./challengeProgress";
--- language-learning-app/scripts/seed.ts
+++ language-learning-app/scripts/seed.ts
@@ -13,6 +13,11 @@ const main = async () => {
     console.log("Seeding the database");
     await db.delete(schema.userProgress);
     await db.delete(schema.courses);
+    await db.delete(schema.units);
+    await db.delete(schema.lessons);
+    await db.delete(schema.challenges);
+    await db.delete(schema.challengeOptions);
+    await db.delete(schema.challengeProgress);
 
     await db.insert(schema.courses).values([
       {
@@ -34,6 +39,45 @@ const main = async () => {
       {
         title: "Croatian",
         imageSrc: "/hr.svg",
+      },
+    ]);
+
+    await db.insert(schema.units).values([
+      {
+        courseId: 1,
+        title: "Unit 1 - Basics",
+        description: "Learn the basics of Spanish",
+        order: 1,
+      },
+      {
+        courseId: 1,
+        title: "Unit 2 - Phrasal Verbs",
+        description: "Learn phrasal verbs in Spanish",
+        order: 2,
+      },
+      {
+        courseId: 2,
+        title: "Unit 1 - Basics",
+        description: "Learn the basics of French",
+        order: 1,
+      },
+      {
+        courseId: 3,
+        title: "Unit 1 - Basics",
+        description: "Learn the basics of Italian",
+        order: 1,
+      },
+      {
+        courseId: 4,
+        title: "Unit 1 - Basics",
+        description: "Learn the basics of Japanese",
+        order: 1,
+      },
+      {
+        courseId: 5,
+        title: "Unit 1 - Basics",
+        description: "Learn the basics of Croatian",
+        order: 1,
       },
     ]);
 
