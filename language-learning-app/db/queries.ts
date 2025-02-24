0b093080ece4a9cc26c754a207aa87f008f4f549
chinel <nelokwaz@gmail.com>
2025-02-25 00:51:16 +0100

    feat: add query logic to get lesson

--- language-learning-app/db/queries.ts
+++ language-learning-app/db/queries.ts
@@ -2,7 +2,13 @@ import { cache } from "react";
 import db from "./drizzle";
 import { auth } from "@clerk/nextjs/server";
 import { eq } from "drizzle-orm";
-import { challengeProgress, courses, units, userProgress } from "./schema";
+import {
+  challengeProgress,
+  courses,
+  lessons,
+  units,
+  userProgress,
+} from "./schema";
 
 export const getUserProgress = cache(async () => {
   try {
@@ -167,19 +173,13 @@ export const getCourseProgress = cache(async () => {
     const firstUncompletedLesson = unitsInActiveCourse
       .flatMap((unit) => unit.lessons)
       .find((lesson) => {
-        return (
-          lesson.challenges.some((challenge) => {
-            return (
-              !challenge.challengeProgress ||
-              challenge.challengeProgress.length === 0 ||
-              !challenge.challengeProgress.every(
-                (progress) => progress.completed
-              )
-            );
-          }) ||
-          !lesson.challenges ||
-          lesson.challenges.length === 0
-        );
+        return lesson.challenges.some((challenge) => {
+          return (
+            !challenge.challengeProgress ||
+            challenge.challengeProgress.length === 0 ||
+            !challenge.challengeProgress.every((progress) => progress.completed)
+          );
+        });
       });
 
     return {
@@ -188,6 +188,50 @@ export const getCourseProgress = cache(async () => {
     };
   } catch (error) {
     console.error("Error fetching course progress:", error);
+    return null;
+  }
+});
+
+export const getLesson = cache(async (id?: number) => {
+  try {
+    const { userId } = await auth();
+    const courseProgress = await getCourseProgress();
+
+    const lessonId = id || courseProgress?.activeLessonId;
+
+    if (!userId || !courseProgress?.activeLessonId || !lessonId) {
+      return null;
+    }
+
+    const lesson = await db.query.lessons.findFirst({
+      where: eq(lessons.id, lessonId),
+      with: {
+        challenges: {
+          orderBy: (challenges, { asc }) => [asc(challenges.order)],
+          with: {
+            challengeOptions: true,
+            challengeProgress: {
+              where: eq(challengeProgress.userId, userId),
+            },
+          },
+        },
+      },
+    });
+
+    if (!lesson || !lesson.challenges) {
+      return null;
+    }
+
+    const normalizedChallenges = lesson.challenges.map((challenge) => {
+      const completed =
+        challenge.challengeProgress &&
+        challenge.challengeProgress.length > 0 &&
+        challenge.challengeProgress.every((progress) => progress.completed);
+      return { ...challenge, completed };
+    });
+    return { ...lesson, challenges: normalizedChallenges };
+  } catch (error) {
+    console.error("Error fetching lesson:", error);
     return null;
   }
 });
