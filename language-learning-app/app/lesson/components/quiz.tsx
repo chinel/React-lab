a22bb5ced097b544142d0d568948e0aad8d6aaed
chinel <nelokwaz@gmail.com>
2025-03-04 23:57:09 +0100

    feat: create question bubble & extend quiz component

--- language-learning-app/app/lesson/components/quiz.tsx
+++ language-learning-app/app/lesson/components/quiz.tsx
@@ -3,6 +3,7 @@
 import { challengeOptions, challenges } from "@/db/schema";
 import { useState } from "react";
 import Header from "./header";
+import QuestionBubble from "./question-bubble";
 
 type Props = {
   intitialLessonId: number;
@@ -24,6 +25,19 @@ const Quiz = ({
 }: Props) => {
   const [hearts, setHearts] = useState(initialHearts);
   const [percentage, setPercentage] = useState(initialPercentage);
+  const [challenges, setChallenges] = useState(initialLessonChallenges);
+  const [activeIndex, setActiveIndex] = useState(() => {
+    const uncompletedIndex = challenges.findIndex(
+      (challenge) => !challenge.completed
+    );
+    return uncompletedIndex === -1 ? 0 : uncompletedIndex;
+  });
+  const challenge = challenges[activeIndex];
+
+  const title =
+    challenge.type === "ASSIST"
+      ? "Select the correct meaning"
+      : challenge.question;
   return (
     <>
       <Header
@@ -31,6 +45,21 @@ const Quiz = ({
         percentage={percentage}
         hasActiveSubscription={!!userSubscription?.isActive}
       />
+      <div className="flex-1">
+        <div className="h-full flex items-center justify-center">
+          <div className="lg:min-h-[350px] lg:w-[600px] w-full px-6 lg:px-0 flex flex-col gap-y-12">
+            <h1 className="text-lg lg:text-3xl text-center lg:text-start font-bold text-neutral-700">
+              {title}
+            </h1>
+            <div>
+              {/**Change back to type assist */}
+              {challenge.type === "SELECT" && (
+                <QuestionBubble question={challenge.question} />
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
