4f23168f8bb6454a46dd2aa7d505993a82e9fbf3
chinel <nelokwaz@gmail.com>
2025-03-05 19:45:16 +0100

    feat: create card and challenge component

---
+++ language-learning-app/app/lesson/components/challenge.tsx
@@ -0,0 +1,52 @@
+import { challengeOptions, challenges } from "@/db/schema";
+import { cn } from "@/lib/utils";
+import Card from "./card";
+
+type Props = {
+  options: (typeof challengeOptions.$inferInsert)[];
+  onSelect: (id?: number) => void;
+  status: "correct" | "wrong" | "none";
+  selectedOption?: number;
+  disabled?: boolean;
+  type: (typeof challenges.$inferSelect)["type"];
+};
+const Challenge = ({
+  options,
+  onSelect,
+  status,
+  selectedOption,
+  disabled,
+  type,
+}: Props) => {
+  return (
+    <>
+      {/**Make sure there is no space in the square bracket grid-cols-[repeat] else it won't work */}
+      <div
+        className={cn(
+          "grid gap-2",
+          type === "ASSIST" && "grid-cols-1",
+          type === "SELECT" &&
+            "grid-cols-2 lg:grid-cols-[repeat(auto-fit,minmax(0,1fr))]"
+        )}
+      >
+        {options.map((option, i) => (
+          <Card
+            key={option.id}
+            id={option.id}
+            text={option.text}
+            imageSrc={option.imageSrc}
+            shortcut={`${i + 1}`}
+            selected={selectedOption === option.id}
+            onClick={() => onSelect(option.id)}
+            status={status}
+            audioSrc={option.audioSrc}
+            disabled={disabled}
+            type={type}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Challenge;
--- language-learning-app/app/lesson/components/index.tsx
+++ language-learning-app/app/lesson/components/index.tsx
@@ -1,3 +1,5 @@
 export { default as Quiz } from "./quiz";
 export { default as Header } from "./header";
 export { default as QuestionBubble } from "./question-bubble";
+export { default as Challenge } from "./challenge";
+export { default as Card } from "./card";
